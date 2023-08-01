c70c1cd462cb4be01131150cd052093edf5dbc54
costa <costa@hellotalk.com>
2023-08-01 17:46:08 +0800

    feat: init code

---
+++ lua/plugins/git-conflict.lua
@@ -0,0 +1,7 @@
+-- https://github.com/akinsho/git-conflict.nvim
+-- lazy.nvim
+return {
+  "akinsho/git-conflict.nvim",
+  version = "*",
+  config = true,
+}
---
+++ lua/plugins/go.lua
@@ -0,0 +1,35 @@
+return {
+  "ray-x/go.nvim",
+  -- 依赖
+  dependencies = {
+    "ray-x/guihua.lua",
+    "neovim/nvim-lspconfig",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  --  配置
+  config = function()
+    require("go").setup()
+  end,
+  event = { "CmdlineEnter" },
+  ft = { "go", "gomod" },
+  build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+  opts = {
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = "*.go",
+      callback = function()
+        require("go.format").goimport()
+      end,
+      -- group = vim.api.nvim_create_augroup("GoFormat", { "f" }),
+    }),
+  },
+  keys = {
+
+    {
+      "<leader>cgh",
+      function()
+        require("go.godoc").run()
+      end,
+      desc = "Go Doc",
+    },
+  },
+}
