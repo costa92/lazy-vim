39ed6bfec666dea773b729d0570189ec49730e74
costa <costa@hellotalk.com>
2023-08-16 16:40:28 +0800

    fix: fix code

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -19,7 +19,7 @@ return {
       callback = function()
         require("go.format").goimport()
       end,
-      group = vim.api.nvim_create_augroup("GoFormat", { "f" }),
+      -- group = vim.api.nvim_create_augroup("GoFormat", { "*" }),
     }),
   },
   keys = {
@@ -30,5 +30,58 @@ return {
       end,
       desc = "Go Doc",
     },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        gopls = {
+          settings = {
+            gopls = {
+              analyses = {
+                unusedparams = true,
+              },
+              hints = {
+                assignVariableTypes = true,
+                compositeLiteralFields = true,
+                compositeLiteralTypes = true,
+                constantValues = true,
+                functionTypeParameters = true,
+                parameterNames = true,
+                rangeVariableTypes = true,
+              },
+              staticcheck = true,
+              semanticTokens = true,
+            },
+          },
+        },
+        golangci_lint_ls = {},
+      },
+      setup = {
+        gopls = function(_, _)
+          local lsp_utils = require("base.lsp.utils")
+          lsp_utils.on_attach(function(client, bufnr)
+            local map = function(mode, lhs, rhs, desc)
+              if desc then
+                desc = desc
+              end
+              vim.keymap.set(mode, lhs, rhs, { silent = true, desc = desc, buffer = bufnr, noremap = true })
+            end
+            -- stylua: ignore
+            if client.name == "gopls" then
+              map("n", "<leader>ly", "<cmd>GoModTidy<cr>", "Go Mod Tidy")
+              map("n", "<leader>lc", "<cmd>GoCoverage<Cr>", "Go Test Coverage")
+              map("n", "<leader>lt", "<cmd>GoTest<Cr>", "Go Test")
+              map("n", "<leader>lR", "<cmd>GoRun<Cr>", "Go Run")
+              map("n", "<leader>dT", "<cmd>lua require('dap-go').debug_test()<cr>", "Go Debug Test")
+            end
+          end)
+        end,
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = { "leoluz/nvim-dap-go", opts = {} },
   },
 }
