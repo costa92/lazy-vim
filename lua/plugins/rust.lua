537f928ca7ffe8c0909c437489032ec761dd8438
costa92 <944310536@qq.com>
2023-09-02 16:42:20 +0800

    fix: rust

---
+++ lua/plugins/rust.lua
@@ -0,0 +1,20 @@
+return {
+  { import = "lazyvim.plugins.extras.lang.rust" },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "simrat39/rust-tools.nvim",
+    },
+    opts = {
+      servers = {
+        rust_analyzer = {},
+      },
+      setup = {
+        rust_analyzer = function(_, opts)
+          require("rust-tools").setup({ server = opts })
+          return true
+        end,
+      },
+    },
+  },
+}
