9dd0243fbd14b2ca68620d4ce3a6f248fef7d6a8
costa <costa@helltalk.com>
2025-04-14 16:21:08 +0800

    chore: 更新 lazy-lock.json 中插件版本并添加 vim-fugitive 插件

--- lua/configs/lazy.lua
+++ lua/configs/lazy.lua
@@ -51,7 +51,8 @@ require("lazy").setup({
     { import = "plugins/notify" },
     { import = "plugins/avante" }, 
     { import = "plugins/root" },   
-    { import = "plugins/blame" },   
+    { import = "plugins/blame" },  
+    { import = "plugins/vim-fugitive" },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
---
+++ lua/plugins/vim-fugitive.lua
@@ -0,0 +1,7 @@
+return {
+  "tpope/vim-fugitive",
+  cmd = { "Git", "G", "Gvdiffsplit" },
+  dependencies = {
+    "tpope/vim-rhubarb", -- 用于 GitHub 集成
+  },
+}
