4a9c817a54618a0747dd1f2443b4be0af502755a
costa <costa@helltalk.com>
2025-05-06 17:52:17 +0800

    docs: 在文件导航中新增快捷键以显示和复制当前文件的相对路径，提升用户操作便利性

--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -74,3 +74,9 @@ end, { desc = 'Toggle line numbers' })
 
 -- vim.keymap.set("i", "<C-BS>", "<C-W>")
 -- vim.keymap.set("i", "<C-H>", "<C-W>")
+
+vim.keymap.set("n", "<leader>fp", ':echo expand("%:p")<CR>', { desc = "显示当前文件路径" })
+vim.keymap.set("n", "<leader>yfp", [[:let @+ = expand("%:p")<CR>]], { desc = "复制当前文件绝对路径" })
+-- 相对路径
+vim.keymap.set("n", "<leader>fr", ':echo expand("%")<CR>', { desc = "显示当前文件路径" })
+vim.keymap.set("n", "<leader>yr", [[:let @+ = expand("%")<CR>]], { desc = "复制当前文件相对路径" })
