1965ae71ab8e3bc4acae414abe026753097d4761
costa <costa@helltalk.com>
2025-05-06 18:09:38 +0800

    docs: 更新快捷键说明以使用 ctrl-t 打开浮动终端，提升用户操作便利性

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,8 +2,8 @@ return {
   "akinsho/toggleterm.nvim",  
   config = function()  
     require("toggleterm").setup({  
-      open_mapping = [[<c-\>]],  -- ✅ 快捷键语法正确  
-      start_in_insert = true,     -- ⚠️ 这里必须加逗号  
+      open_mapping = [[<c-t>]],  -- 改用 ctrl-t 作为快捷键
+      start_in_insert = true,     
       direction = "float"    -- tab, float, vertical
     })  
   end  
