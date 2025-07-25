621dde95f7805e453ecc293e130809e0c0732218
costa <costa@helltalk.com>
2025-07-25 16:19:00 +0800

    refactor(keymaps): 修改 K 键快捷键，避免冲突

--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -21,7 +21,8 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Switch Right Window" })
 
 -- 上下移动选中文本
 vim.keymap.set("v", "j", ":move '>+1<CR>gv-gv", opt)
-vim.keymap.set({"n", "i"}, "K", ":move '<-2<CR>gv-gv", opt)
+-- 修改：将 K 键改为 Alt+k，避免冲突
+vim.keymap.set({"n", "i"}, "<A-k>", ":move '<-2<CR>gv-gv", opt)
 
 -- insert 模式下，跳到行首行尾
 vim.keymap.set("i", "<C-h>", "<ESC>I", opt)
@@ -37,40 +38,40 @@ vim.keymap.set({ "i", "x", "n", "s" }, "<C-q>", vim.cmd.quit, { desc = "Quit Fil
 vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", vim.cmd.write, { desc = "Save File" })
 
 -- Go IDE
--- 绑定 Ctrl+i 快捷键执行 GoFillStruct  
--- vim.keymap.set("n", "<C-i>", ":GoFillStruct<CR>", { desc = "Fill Struct in Go" })  
-vim.keymap.set("n", "<leader>fe", ":GoIfErr<CR>", { desc = "Fill Struct in Go" })  
-vim.keymap.set("n", "<leader>fs", ":GoFillStruct<CR>", { desc = "Fill Struct in Go" })  
-vim.keymap.set("n", "<leader>fc", ":GoFillSwitch<CR>", { desc = "Fill Struct in Go" })  
-vim.keymap.set("n", "<leader>ta", ":GoAddTag<CR>", { desc = "Fill Struct in Go" })  
-vim.keymap.set("n", "<leader>tr", ":GoRmTag<CR>", { desc = "Fill Struct in Go" })  
-vim.keymap.set("n", "<leader>tc", ":GoClearTag<CR>", { desc = "Fill Struct in Go" })  
-vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)  
-vim.keymap.set("n", "gd", "<cmd>FzfLua lsp_definitions<CR>", { desc = "Go to Definition" })  
-vim.keymap.set("n", "gr", "<cmd>FzfLua lsp_references<CR>", { desc = "Go to Definition" })  
-vim.keymap.set("n", "gi", "<cmd>FzfLua lsp_implementations<CR>", { desc = "Go to Definition" })  
-vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)  
+-- 绑定 Ctrl+i 快捷键执行 GoFillStruct
+-- vim.keymap.set("n", "<C-i>", ":GoFillStruct<CR>", { desc = "Fill Struct in Go" })
+vim.keymap.set("n", "<leader>fe", ":GoIfErr<CR>", { desc = "Fill Struct in Go" })
+vim.keymap.set("n", "<leader>fs", ":GoFillStruct<CR>", { desc = "Fill Struct in Go" })
+vim.keymap.set("n", "<leader>fc", ":GoFillSwitch<CR>", { desc = "Fill Struct in Go" })
+vim.keymap.set("n", "<leader>ta", ":GoAddTag<CR>", { desc = "Fill Struct in Go" })
+vim.keymap.set("n", "<leader>tr", ":GoRmTag<CR>", { desc = "Fill Struct in Go" })
+vim.keymap.set("n", "<leader>tc", ":GoClearTag<CR>", { desc = "Fill Struct in Go" })
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+vim.keymap.set("n", "gd", "<cmd>FzfLua lsp_definitions<CR>", { desc = "Go to Definition" })
+vim.keymap.set("n", "gr", "<cmd>FzfLua lsp_references<CR>", { desc = "Go to Definition" })
+vim.keymap.set("n", "gi", "<cmd>FzfLua lsp_implementations<CR>", { desc = "Go to Definition" })
+vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 
 -- FZF-Lua
-vim.keymap.set("n", "<C-e>", "<cmd>FzfLua buffers<CR>", { desc = "buffers" })  
+vim.keymap.set("n", "<C-e>", "<cmd>FzfLua buffers<CR>", { desc = "buffers" })
 vim.keymap.set("n", "<leader>r", "<cmd>FzfLua oldfiles<CR>", { desc = "mru" })   --mru: most recent used
 vim.keymap.set("n", "<leader>s", "<cmd>FzfLua treesitter<CR>", { desc = "mru" })   --mru: most recent used
-vim.keymap.set("n", "<leader>f", "<cmd>FzfLua live_grep<CR>", { desc = "lines" })  
-vim.keymap.set("n", "<leader>h", "<cmd>FzfLua search_history<CR>", { desc = "lines" })  
-vim.keymap.set("n", "<leader>m", "<cmd>FzfLua marks<CR>", { desc = "lines" })  
+vim.keymap.set("n", "<leader>f", "<cmd>FzfLua live_grep<CR>", { desc = "lines" })
+vim.keymap.set("n", "<leader>h", "<cmd>FzfLua search_history<CR>", { desc = "lines" })
+vim.keymap.set("n", "<leader>m", "<cmd>FzfLua marks<CR>", { desc = "lines" })
 vim.keymap.set("n", "<leader>o", "<cmd>FzfLua files<CR>", { desc = "Open file" })  -- 新增：快速打开文件
-vim.keymap.set("n", "<leader>gp", "<cmd>FzfLua git_commits<CR>", { desc = "lines" })  
-vim.keymap.set("n", "<leader>gb", "<cmd>FzfLua git_bcommits<CR>", { desc = "lines" })  
-vim.keymap.set("n", "<leader>gs", "<cmd>FzfLua git_status<CR>", { desc = "lines" })  
-vim.keymap.set("n", "<C-f>", "<cmd>FzfLua lgrep_curbuf<CR>", { desc = "lines" })  
+vim.keymap.set("n", "<leader>gp", "<cmd>FzfLua git_commits<CR>", { desc = "lines" })
+vim.keymap.set("n", "<leader>gb", "<cmd>FzfLua git_bcommits<CR>", { desc = "lines" })
+vim.keymap.set("n", "<leader>gs", "<cmd>FzfLua git_status<CR>", { desc = "lines" })
+vim.keymap.set("n", "<C-f>", "<cmd>FzfLua lgrep_curbuf<CR>", { desc = "lines" })
 
 -- Git
-vim.keymap.set("n", "<leader>b", "<cmd>BlameToggle<CR>", { desc = "lines" })  
+vim.keymap.set("n", "<leader>b", "<cmd>BlameToggle<CR>", { desc = "lines" })
 
 -- 其他
-vim.keymap.set('n', '<S-n>', function()  
-  vim.wo.number = not vim.wo.number  
-end, { desc = 'Toggle line numbers' })  
+vim.keymap.set('n', '<S-n>', function()
+  vim.wo.number = not vim.wo.number
+end, { desc = 'Toggle line numbers' })
 
 -- vim.keymap.set("i", "<C-BS>", "<C-W>")
 -- vim.keymap.set("i", "<C-H>", "<C-W>")
